7b26800242d9d84a10201810b2257caee3d3faea
tromao <tiagomromao@gmail.com>
2023-11-10 21:56:13 +0000

    added sqldelight plugin and library

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+
+    alias(libs.plugins.sqldelight).apply(false)
 }
